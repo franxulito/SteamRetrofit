ee12638d306d0cbb2ec09de1ff6c905320f02342
Agustín Sánchez Martín <alumno.98429@ies-azarquiel.es>
2018-02-05 11:43:36 +0100

    This is the frist commit

--- app/src/main/java/net/azarquiel/steamretrofixrx/MainActivity.kt
+++ app/src/main/java/net/azarquiel/steamretrofixrx/MainActivity.kt
@@ -24,7 +24,7 @@ import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        val TAG = "**Paco**"
+        val TAG = "**Paco***"
     }
 
     private lateinit var games:Games
